959b89c695f5819965a7b7760b7ab8c986b9382b
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-05-19 23:47:48 +0600

    gitignore file created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 
 const app = express();
@@ -27,6 +27,7 @@ async function run() {
 
     const db = client.db("medi-queue");
     const tutorCollection = db.collection("tutors");
+    const BookedSessionsCollection = db.collection("booked-sessions");
 
     app.get("/tutors", async (req, res) => {
       const result = await tutorCollection.find().toArray();
@@ -41,6 +42,23 @@ async function run() {
       const newTutor = req.body;
       console.log(newTutor, "newTutor is");
       const result = await tutorCollection.insertOne(newTutor);
+      res.send(result);
+    });
+
+    app.get("/tutors/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tutorCollection.findOne(query);
+      res.send(result);
+    })
+    app.get("/booked-sessions", async (req, res) => {
+      const result = await BookedSessionsCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/booked-sessions", async (req, res) => {
+      const newSession = req.body;
+      console.log(newSession, "newSession is");
+      const result = await BookedSessionsCollection.insertOne(newSession);
       res.send(result);
     });
 
